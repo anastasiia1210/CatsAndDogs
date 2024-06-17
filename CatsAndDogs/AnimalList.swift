785f3c6cb4d34c92eb9a735f1b5477e6aa27a962
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2024-06-17 21:50:33 +0300

    bitrise

--- CatsAndDogs/AnimalList.swift
+++ CatsAndDogs/AnimalList.swift
@@ -25,6 +25,7 @@ struct AnimalList: View {
         .padding()
         .onAppear {
           var url = "https://api.thecatapi.com/v1/images/search?limit=10"
+            
             let dogsOrCats = Bundle.main.object(forInfoDictionaryKey: "ANIMALS") as? String ?? ""
             if (dogsOrCats == "CATS") {url = "https://api.thecatapi.com/v1/images/search?limit=10"}
             if (dogsOrCats == "DOGS") {url = "https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=10"}
